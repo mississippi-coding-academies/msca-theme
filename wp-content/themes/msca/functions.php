5647e490ee88d6c6115003e40859444c3873d857
Bryce Mullican <bryce.mullican@gmail.com>
2020-02-13 14:00:45 -0600

    added custom columns to student admin

--- wp-content/themes/msca/functions.php
+++ wp-content/themes/msca/functions.php
@@ -11,3 +11,31 @@ function register_menus() {
     ]);
 }
 add_action('init', 'register_menus');
+
+add_filter( 'manage_edit-student_columns', 'my_edit_student_columns' ) ;
+function my_edit_student_columns( $columns ) {
+
+	$columns['title'] = __( 'Student Name' );
+	$columns['active'] = __( 'Active' );
+	$columns['cohort'] = __( 'Cohort' );
+
+	return $columns;
+}
+
+add_action( 'manage_student_posts_custom_column', 'my_manage_student_columns', 10, 2 );
+
+function my_manage_student_columns( $column, $post_id ) {
+	global $post;
+
+	switch( $column ) {
+
+		case 'active' :
+            echo get_post_meta( $post_id , 'active' , true ); 
+            break;
+
+        case 'cohort' :
+            echo get_post_meta( $post_id , 'cohort' , true ); 
+            break;
+
+    }
+}
