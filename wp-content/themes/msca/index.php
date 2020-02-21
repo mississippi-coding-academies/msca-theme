f8dbb6d628e6a71efd652b9cc86711f9f159ec79
Bryce Mullican <bryce.mullican@gmail.com>
2020-02-21 12:54:28 -0600

    updated news page

---
+++ wp-content/themes/msca/index.php
@@ -0,0 +1,31 @@
+<?php
+    // index.php
+    get_header();
+?>
+<!-- Page Header -->
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>News</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-bottom">
+        <div class="container">
+            <div class="about-sub-header">
+                <div class="row">
+                    <div class="col-12">
+<?php while(have_posts()) : the_post(); ?>
+                        <article class="news-item">
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                        </article>
+<?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php get_footer(); ?>
