ab4b38a8c72e44fcf3bd38395988b3979f143405
Bryce Mullican <bryce.mullican@gmail.com>
2020-03-03 14:39:24 -0600

    working on students page

---
+++ wp-content/themes/msca/page-students.php
@@ -0,0 +1,95 @@
+<?php
+    // page.php
+    get_header();
+?>
+<!-- Page Header -->
+    <?php while(have_posts()) : the_post(); ?>
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endwhile; ?>
+    <div class="alumni-sub-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <form action="#">
+                        <label for="location">Filter Location</label>
+                        <input type="checkbox" name="Jackson" id="" value="Jackson" checked>Jackson
+                        <input type="checkbox" name="Starkville" id="" value="Starkville">Starkville
+                    </form>
+                </div>
+                <div class="col-6">
+                    <form action="#">
+                        <label for="sort">Sort Students By</label>
+                        <select name="sort-options">
+                            <option value="location">Location</option>
+                            <option value="year">Year</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="alumni-bg">
+        <div class="container">
+            <div class="row">
+            <?php
+
+global $post;
+$args = [
+    'post_type' => 'student',
+    'meta_query' => [
+        [
+            'key' => 'active',
+            'value' => '1',
+            'compare' => '='
+        ]
+    ]
+];
+
+$myposts = get_posts( $args );
+foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+<!-- OPEN STUDENT -->
+                <div class="col-3">
+                    <div class="alumni-box">
+                        <div class="alumni-photo-1">
+                            <img src="https://via.placeholder.com/100x100" alt="placeholder image">
+                        </div>
+                        <div class="row">
+                            <div class="col-8">
+                                <p><?php the_title(); ?><br> <?php echo ucfirst(get_field('location'));?></p>
+                            </div>
+                            <div class="col-4">
+                                <a href="#">X</a>
+                                <a href="#">X</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<!-- CLOSE STUDENT -->
+<?php 
+            endforeach; 
+            wp_reset_postdata();
+        ?>
+            </div>
+        </div>
+    </div>
+    <div class="footer-message">
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    <p>If you’re a problem solver with ambition—we have a place for you.</p>
+                </div>
+                <div class="col-5">
+                    <a href="/apply-now/">Apply to the Academy</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php get_footer(); ?>
