1ad1885940c11092011776c2602c69d054267982
Bryce Mullican <bryce.mullican@gmail.com>
2020-05-28 14:11:07 -0500

    added a default image to news page

--- wp-content/themes/msca/index.php
+++ wp-content/themes/msca/index.php
@@ -21,6 +21,8 @@
                         <article class="news-item">
                         <?php if ( has_post_thumbnail() ) : ?>
                             <?php echo get_the_post_thumbnail( $post->ID, 'standard-horizontal' ); ?>
+                        <?php else :  ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/default_news.jpg" alt="Mississippi Coding Academies">
                         <?php endif; ?>
                             <h2 class="h3"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
                             <?php the_excerpt(); ?>
